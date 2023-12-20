0207ce3a8d1f06883dfb3afb85f775964bf81bbb
Celiix <92170983+Celiix@users.noreply.github.com>
2023-12-20 20:16:35 +0000

    Update savedata.php - fix error to stop data from overwriting existing data

--- savedata.php
+++ savedata.php
@@ -3,9 +3,18 @@ $data = json_decode(file_get_contents('php://input'), true);
 
 if ($data) {
     $filename = 'flashcards.txt';
-    file_put_contents($filename, json_encode($data));
-    echo 'Data saved successfully!';
+      $existingData = json_decode(file_get_contents($filename), true);
+
+     // Add new data to existing data
+   if ($existingData) {
+    $data = array_merge($existingData, $data);
+   }
+    
+ //Updated data save into the file
+   file_put_contents($filename, json_encode($data, JSON_PRETTY_PRINT));
+
+   echo 'Data saved!';
 } else {
-    echo 'Error: Invalid data.';
-}
+    echo 'Error! Data is invalid';
+}   
 ?>
